dd9fee91353857533f3b949e5696503871bcb8d2
fedir72 <fedir72@ua.fm>
2020-06-10 12:55:57 +0300

    + goToSafariVC

--- newsapi_App/newsapi_App/OneNewsController.swift
+++ newsapi_App/newsapi_App/OneNewsController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class OneNewsController: UIViewController {
     
@@ -15,6 +16,7 @@ class OneNewsController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var openInBrowserButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +29,21 @@ class OneNewsController: UIViewController {
                 if let data = try? Data(contentsOf: url) { self.imageView.image = UIImage(data: data ) }
                  }
              }
+       
+        
+        if URL(string: (article.url!)) == nil {
+            self.openInBrowserButton.isHidden = true
         }
+        
+        
+    }
      
     @IBAction func pushOpenAction(_ sender: UIButton) {
-     
+        if let url = URL(string: (article.url!)) {
+        let svc = SFSafariViewController(url: url)
+        present(svc, animated: true, completion: nil)
+        }
     }
-    
+   
 
 }
